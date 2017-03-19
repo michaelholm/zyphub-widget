f386fd762a3d04f4a1de61fa758b8369483bb631
Michael Holm <michael.holm@gmail.com>
2017-03-19 12:05:41 -0500

    Navbar
    Removed old file, added navbar

---
+++ app/src/components/navbar.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <nav class="nav has-shadow" >
+    <div class="nav-left">
+      <a class="nav-item is-brand" to="/">
+        ZypHub
+      </a>
+      <a class="nav-item is-tab" to="/dashboard">
+        <i class="fa fa-dashboard"></i> Dashboard
+      </a>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'navbar'
+  }
+</script>
